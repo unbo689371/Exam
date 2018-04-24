9823ad164b2dd6de648dea8e398930dc49c855b0
unbo689371 <33127539+unbo689371@users.noreply.github.com>
2018-04-24 11:57:25 +0800

    Update FactorialSum.py

--- FactorialSum.py
+++ FactorialSum.py
@@ -4,4 +4,17 @@ number = factorial(100)
 sum = 0
 for digit in str(number):
     sum = sum + int(digit)
-print(sum)
+print(sum) 
+
+
+
+a1 = 1
+a2 = 1
+a3 = a1 + a2
+while a3<4000000:
+    if a3%2==0:
+        
+        a3 = a1 + a2
+        a1 = a2
+        a2 = a3
+        
