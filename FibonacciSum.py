d9d72a7b6bb8b7d75d4d2822b864ce480e3901ec
unbo689371 <33127539+unbo689371@users.noreply.github.com>
2018-04-30 19:37:06 +0800

    Add files via upload

---
+++ FibonacciSum.py
@@ -0,0 +1,30 @@
+nterms = int(input("你需要幾項?"))
+
+n1 = 0
+n2 = 1
+count = 2
+
+if nterms <=0:
+    print("請輸入一個正整數。")
+elif nterms == 1:
+    print("婓波那契數列:")
+    print(n1)
+else:
+    print("斐波那契数列：")
+    print(n1,",",n2,end=" , ")
+    while count < nterms:
+        nth = n1 + n2
+        print(nth,end=" , ")
+
+        n1 = n2
+        n2 = nth
+        count += 1
+
+
+if nth%2 == 0 or n1%2==0 or n2%2 ==0:
+    sum = 0
+    sum = sum + nth
+    print(sum)
+else:
+    print('奇數')
+        
