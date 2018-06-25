427e1ddd950d58215fbdf92b18c287edb098fa08
unbo689371 <33127539+unbo689371@users.noreply.github.com>
2018-06-25 21:33:48 +0800

    Add files via upload
    期末考

---
+++ ex7prime10001.py
@@ -0,0 +1,15 @@
+import math
+def prime(n):
+    m=int(math.sqrt(n))+1
+    for i in range(2,m):
+        if n%i==0:
+            return False
+    return True
+count=0
+i=2
+while count<10001:
+    if prime(i):
+        count=count+1
+    if count!=10001:
+        i=i+1
+print(i)
