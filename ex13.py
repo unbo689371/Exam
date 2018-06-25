878976ddac2b3a40155b21369fa9158dbb2920a6
unbo689371 <33127539+unbo689371@users.noreply.github.com>
2018-06-26 00:30:31 +0800

    Add files via upload

--- ex13.py
+++ ex13.py
@@ -7,9 +7,8 @@ file = open('D:/python/n.txt', 'r')
 for line in file.readlines():    #依序讀取每行 
     line = line.strip()    #去掉每行頭尾空白 
     numbers.append(int(line)) 
-
 for x in range(0,len(numbers)): 
-    asdf=int(numbers[x])>>40 
+    asdf=int(int(numbers[x])/10000000000000000000000000000000000000000) 
     count_num=count_num+int(asdf) 
 # 關閉檔案 
 print(count_num) 
