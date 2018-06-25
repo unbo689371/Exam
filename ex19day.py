c82e8e00b0368abf049bfcdfeda76379950cf9a0
unbo689371 <33127539+unbo689371@users.noreply.github.com>
2018-06-25 21:43:02 +0800

    Add files via upload

---
+++ ex19day.py
@@ -0,0 +1,8 @@
+import datetime
+sum = 0 
+for y in range(1901,2001,1):
+    for m in range(1, 13, 1):
+        day = datetime.datetime(y, m, 1)
+        if day.weekday() == 6:
+            sum = sum + 1
+print(sum)
